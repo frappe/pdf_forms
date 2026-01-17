d508e0f26cba1db74c99388ba25bd2fa48d164e4
Sumit Jain <jainmit23@gmail.com>
2026-01-17 12:25:50 +0530

    feat: Add document handling and context management functionality

--- documents/src/App.tsx
+++ documents/src/App.tsx
@@ -1,18 +1,18 @@
 import { FrappeProvider } from 'frappe-react-sdk'
-import { Navigate, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import { UserProvider } from '@/providers/UserProvider'
 import { Toaster } from 'sonner'
+import { useEffect } from 'react'
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
-	  <>
-		<Route path='/login' element={<Navigate to="/" />} />
-		<Route path='*' element={<></>} />
-	  </>
+		<>
+			<Route path='/' element={<><h1>Documents</h1></>} />
+		</>
 	), {
 	basename: import.meta.env.VITE_BASE_NAME ? `/${import.meta.env.VITE_BASE_NAME}` : '',
-  }
-  )
+}
+)
 
 function App() {
 
@@ -26,6 +26,22 @@ function App() {
 			return window.frappe?.boot?.sitename ?? import.meta.env.VITE_SITE_NAME
 		}
 	}
+
+	useEffect(() => {
+		// Check if user is logged in by checking the Cookie "user_id"
+		// In Frappe, unauthenticated users are "Guest"
+		const user = document.cookie?.split('; ').find(row => row.startsWith('user_id='))?.split('=')[1]?.trim()
+		const isLoggedIn = user !== 'Guest'
+
+		if (!isLoggedIn) {
+			if (import.meta.env.DEV) {
+				return
+			}
+			// Redirect to Frappe login page
+			window.location.href = '/login?redirect-to=/documents'
+			return
+		}
+	}, [])
 
 	return (
 		<FrappeProvider
--- documents/src/main.tsx
+++ documents/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+if (import.meta.env.DEV) {
+  fetch('/api/method/doc_mapper.www.documents.get_context_for_dev', {
+    method: 'POST',
+  })
+    .then(response => response.json())
+    .then((values) => {
+      const v = JSON.parse(values.message)
+      // @ts-expect-error expected
+      if (!window.frappe) window.frappe = {};
+
+      // @ts-expect-error expected
+      window.frappe.boot = v;
+    })
+}
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
