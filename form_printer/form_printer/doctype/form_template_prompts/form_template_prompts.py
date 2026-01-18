c302c7ec12869a0198ba1fe60c190d68d1134cd1
Sumit Jain <jainmit23@gmail.com>
2026-01-18 16:32:49 +0530

    feat: Introduce Form Template and Form Template Prompts doctypes with initial configurations and test cases

---
+++ form_printer/form_printer/doctype/form_template_prompts/form_template_prompts.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class FormTemplatePrompts(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		field_name: DF.Data
+		label: DF.Data
+		mandatory: DF.Check
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		question: DF.SmallText | None
+		type: DF.Literal["Text", "Checkbox", "Radio"]
+	# end: auto-generated types
+	pass
